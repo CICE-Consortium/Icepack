eca3b8b3a8b36a1fb4e9422deeed31c408ec9ac8
eclare108213 <eclare@lanl.gov>
2017-09-27 10:41:20 -0600

    Initial Icepack test suite

--- configuration/scripts/tests/base_suite.ts
+++ configuration/scripts/tests/base_suite.ts
@@ -1,12 +1,6 @@
 # Test         Grid    PEs        Sets    BFB-compare
-smoke          gx3     8x2        diag1,run5day
-smoke          gx3     8x2        diag24,run1year,medium
-smoke          gx3     4x1        debug,diag1,run5day
-smoke          gx3     8x2        debug,diag1,run5day
-smoke          gx3     4x2        diag1,run5day          smoke_gx3_8x2_diag1_run5day
-smoke          gx3     4x1        diag1,run5day,thread   smoke_gx3_8x2_diag1_run5day
-smoke          gx3     4x1        diag1,run5day          smoke_gx3_4x1_diag1_run5day_thread
-restart        gx3     8x1        none
-restart        gx3     4x2        debug
-restart        gx3     8x2        diag1,pondcesm
-restart        gx3     8x2        diag1,pondtopo
+smoke          col     1x1        diag1,run5day
+smoke          col     1x1        diag24,run1year,medium
+smoke          col     1x1        debug,diag1,run5day
+restart        col     1x1        debug
+restart        col     1x1        diag1
