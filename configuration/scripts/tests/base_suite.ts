8d2f292a2503e8c4a5a930cc913b448456697104
apcraig <anthony.p.craig@gmail.com>
2017-09-28 18:56:39 -0600

    update the exact restart test.

--- configuration/scripts/tests/base_suite.ts
+++ configuration/scripts/tests/base_suite.ts
@@ -1,6 +1,5 @@
 # Test         Grid    PEs        Sets    BFB-compare
-smoke          col     1x1        diag1,run5day
-smoke          col     1x1        diag24,run1year,medium
-smoke          col     1x1        debug,diag1,run5day
+smoke          col     1x1        diag1,run1year
+smoke          col     1x1        debug,diag1,run1year
 restart        col     1x1        debug
 restart        col     1x1        diag1
