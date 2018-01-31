6c0373ce95dfe7b1633f1f924b7457184d13c731
apcraig <anthony.p.craig@gmail.com>
2018-01-31 22:53:26 +0000

    add quick_suite.ts

---
+++ configuration/scripts/tests/quick_suite.ts
@@ -0,0 +1,4 @@
+# Test         Grid    PEs        Sets    BFB-compare
+smoke          col     1x1        diag1,run1year
+smoke          col     1x1        debug,run1year
+restart        col     1x1        diag1
