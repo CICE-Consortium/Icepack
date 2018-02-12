e25e5314a1108848baa8d79a45369774bbecc57d
apcraig <anthony.p.craig@gmail.com>
2018-02-12 23:27:16 +0000

    fix mechred exp underflows

--- configuration/scripts/tests/travis_suite.ts
+++ configuration/scripts/tests/travis_suite.ts
@@ -1,6 +1,8 @@
 # Test         Grid    PEs        Sets    BFB-compare
 smoke          col     1x1        diag1,run1year
 smoke          col     1x1        debug,run1year
+smoke          col     1x1        debug,bgcISPOL
+smoke          col     1x1        debug,bgcNICE
 smoke          col     1x1        debug,run1year,thermo1
 smoke          col     1x1        debug,run1year,swccsm3
 smoke          col     1x1        debug,run1year,alt01
@@ -11,6 +13,8 @@ restart        col     1x1        diag1
 restart        col     1x1        pondcesm
 restart        col     1x1        pondlvl
 restart        col     1x1        pondtopo
+restart        col     1x1        bgcISPOL
+restart        col     1x1        bgcNICE
 restart        col     1x1        thermo1
 restart        col     1x1        swccsm3
 restart        col     1x1        alt01
