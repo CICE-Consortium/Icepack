2b92358473b5a9eb04c62a286840d2cf07c7bae1
David Bailey <dbailey@ucar.edu>
2020-08-20 09:54:19 -0600

    add units to comment

--- columnphysics/icepack_therm_itd.F90
+++ columnphysics/icepack_therm_itd.F90
@@ -145,7 +145,7 @@
 
       real (kind=dbl_kind) :: &
          slope        , & ! rate of change of dhice with hice
-         denom        , & ! denominator in hbnew calculation
+         denom        , & ! denominator in hbnew calculation (m)
          dh0          , & ! change in ice thickness at h = 0
          da0          , & ! area melting from category 1
          damax        , & ! max allowed reduction in category 1 area
