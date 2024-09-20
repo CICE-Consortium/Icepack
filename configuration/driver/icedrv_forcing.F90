b7270c6404e186df30923781dbdd04e9a3cbef6e
David Bailey <dbailey@ucar.edu>
2024-09-20 16:22:44 -0600

    Fix up some more nx related stuff

--- configuration/driver/icedrv_forcing.F90
+++ configuration/driver/icedrv_forcing.F90
@@ -1137,28 +1137,26 @@
 
       use icedrv_arrays_column, only: wave_spectrum, wave_sig_ht, &
                                    dwavefreq, wavefreq
-      use icedrv_domain_size, only: nfreq, nx
+      use icedrv_domain_size, only: nfreq
 
       ! local variables
       integer (kind=int_kind) :: &
          i,k
 
-      real(kind=dbl_kind), dimension(nx,nfreq) :: &
+      real(kind=dbl_kind), dimension(nfreq) :: &
          wave_spectrum_profile  ! wave spectrum
 
        wave_spectrum(:,:) = c0
 
       ! wave spectrum and frequencies
       ! get hardwired frequency bin info and a dummy wave spectrum profile
-      do i=1,nx
 
       call icepack_init_wave(nfreq=nfreq,                 &
-                             wave_spectrum_profile=wave_spectrum_profile(i,:), &
-                             wavefreq=wavefreq(:), dwavefreq=dwavefreq(:))
-      enddo
+                             wave_spectrum_profile=wave_spectrum_profile, &
+                             wavefreq=wavefreq, dwavefreq=dwavefreq)
 
       do k = 1, nfreq
-          wave_spectrum(:,k) = wave_spectrum_profile(:,k)
+          wave_spectrum(:,k) = wave_spectrum_profile(k)
       enddo
 
       end subroutine get_wave_spec
